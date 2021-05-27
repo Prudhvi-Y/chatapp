729a02082912e941d834832c46c21bd6f5bddc6d
Prudhvi-Y <prudhvi200@gmail.com>
2021-05-26 20:09:06 -0700

    Basic crud functionality is up
    Signed-off-by: Prudhvi-Y <prudhvi200@gmail.com>

--- src/resolvers/postResolver.ts
+++ src/resolvers/postResolver.ts
@@ -1,6 +1,7 @@
-import { MyContext } from "src/types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { MyContext } from "../types";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { PostResponse } from "../reponses/posts";
+
 
 @Resolver()
 export class PostResolver {
@@ -10,7 +11,7 @@ export class PostResolver {
     ): Promise<PostResponse | null> {
 
         const alltitles = await prisma.post.findMany();
-        console.log(alltitles);
+        // console.log(alltitles);
 
         if (alltitles){
             return {
@@ -18,6 +19,121 @@ export class PostResolver {
             };
         } else {
             return {
+                posts: null,
+            };
+        }
+    }
+
+    @Query(() => PostResponse)
+    async singlepost(
+        @Arg("id", () => Int) id: number,
+        @Ctx() {prisma}: MyContext
+    ): Promise<PostResponse | null> {
+
+        const onepost = await prisma.post.findFirst({
+            where: {
+                id: id
+            },
+        });
+
+        if (onepost) {
+            return{
+                posts: [onepost],
+            };
+        } else {
+            return{
+                posts: null,
+            };
+        }
+        
+    }
+
+    @Mutation(() => PostResponse)
+    async createpost (
+        @Arg("title") title: string,
+        @Arg("author") author: string,
+        @Ctx() {prisma}: MyContext
+    ): Promise<PostResponse | null> {
+
+        const created = await prisma.post.create({
+            data: {
+                title: title,
+                author: author,
+            },
+        });
+
+        if (created) {
+            return{
+                posts: [created],
+            };
+        } else {
+            return{
+                posts: null,
+            };
+        }
+    }
+
+    @Mutation(() => PostResponse)
+    async updatepost (
+        @Arg("title") title: string,
+        @Arg("author") author: string,
+        @Arg("id", ()=>Int) id: number,
+        @Ctx() {prisma}: MyContext
+    ): Promise<PostResponse | null> {
+
+        let updated;
+        if (title) {
+            updated = await prisma.post.update({
+                where: {
+                    id: id,
+                },
+                data: {
+                    title: title,
+                },
+            });
+        }
+
+        if (author) {
+            updated = await prisma.post.update({
+                where: {
+                    id: id,
+                },
+                data: {
+                    author: author,
+                },
+            });
+        }
+
+        if (updated) {
+            return{
+                posts: [updated],
+            };
+        } else {
+            return{
+                posts: null,
+            };
+        }
+    }
+
+    @Mutation(() => PostResponse)
+    async deletepost (
+        @Arg("id", () => Int) id: number,
+        @Ctx() {prisma}: MyContext
+    ): Promise<PostResponse | null> {
+
+        try {
+            const deleted = await prisma.post.delete({
+                where: {
+                    id: id,
+                },
+            });
+
+            
+            return{
+                posts: [deleted],
+            };
+        } catch {
+            return{
                 posts: null,
             };
         }
